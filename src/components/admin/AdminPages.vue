c44a1185474fd3db0dac5f40550e997e56be92eb
Regis Seki <regisseki@gmail.com>
2021-04-28 11:22:20 -0300

    Deploy to heroku

---
+++ src/components/admin/AdminPages.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="admim-pages">
+        <PageTitle icon="fa fa-cogs" main="Administração Sistema"
+            sub="Cadastros" />
+        <div class="admin-pages-tab">
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title="Categorias" active>
+                        <CategoryAdmin />
+                    </b-tab>
+                    <b-tab title="Produtos" active>
+                        <ProductAdmin />
+                    </b-tab>
+                    <b-tab title="Usuários" active>
+                        <UserAdmin />
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
+    </div>
+</template>
+
+<script>
+import PageTitle from '@/components/template/PageTitle'
+import CategoryAdmin from './CategoryAdmin'
+import ProductAdmin from './ProductAdmin'
+import UserAdmin from './UserAdmin'
+
+export default {
+    name: 'AdminPages',
+    components: { PageTitle, CategoryAdmin, ProductAdmin, UserAdmin },
+
+}
+</script>
+
+<style>
+    
+</style>
